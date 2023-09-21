e8165fcced4c329c8ce6e4a4652302019aca9756
Danber <steem.sobol@mail.ru>
2023-09-21 17:21:28 -0400

    wrote funny app that allows to put dots in the screen ;)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+interface Points {
+  y: number;
+  x: number;
+}
 
 function App() {
+  const [points, setPoints] = React.useState<Array<Points>>([]);
+  const [cachedPoints, setCachedPoints] = React.useState<Array<Points>>([]);
+
+  const handleAddPoint = (event: React.MouseEvent) => {
+    const { clientY, clientX } = event;
+
+    setPoints((prev) => [...prev, { y: clientY, x: clientX }]);
+  };
+
+  const handleUndoDots = () => {
+    if (!points.length) return;
+
+    const lastPoint = [points[points.length - 1]];
+
+    setCachedPoints((prevState) => [...prevState, ...lastPoint]);
+    setPoints(points.slice(0, points.length - 1));
+  };
+
+  const handleRedoDots = () => {
+    if (!cachedPoints.length) return;
+
+    const lastCachedPoint = [cachedPoints[cachedPoints.length - 1]];
+
+    setCachedPoints(cachedPoints.slice(0, cachedPoints.length - 1));
+    setPoints((prevState) => [...prevState, ...lastCachedPoint]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="buttons">
+        <button onClick={handleUndoDots}>undo</button>
+        <button onClick={handleRedoDots}>redo</button>
+      </div>
+      <div className="board" onClick={handleAddPoint}>
+        {points.map((point, index) => (
+          <div
+            key={index}
+            className="point"
+            style={{ top: `${point.y}px`, left: `${point.x}px` }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
